175cfc64c584ab2c071d499152447476886f755f
Kerstin Bonev-Wagner <k.bonev-wagner@gmx.at>
2022-05-28 13:38:11 +0200

    Context API provides data to components

--- src/App.js
+++ src/App.js
@@ -1,46 +1,34 @@
 import Header from "./components/Header";
-import { v4 as uuidv4 } from "uuid";
-import { useState } from "react";
-import {BrowserRouter as Router, Route, Routes, useLocation} from "react-router-dom";
-import FeedbackData from "./data/FeedbackData";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
 import AboutPage from "./components/pages/AboutPage";
 import AboutIconLink from "./components/pages/AboutIconLink";
+import { FeedbackProvider } from "./components/context/FeedbackContext";
 
 function App() {
-    const [feedback, setFeedback] = useState(FeedbackData)
-
-    const deleteFeedback = (id) => {
-        if(window.confirm("Do you want to delete this item?")) {
-            setFeedback(feedback.filter((item) => item.id !== id));
-        }
-    }
-
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([...feedback, newFeedback]);
-    }
 
     return (
-        <Router>
-            <Header text='Feedback App' />
-            <div className="container">
-                <Routes>
-                    <Route exact path="/" element= {
-                        <>
-                            <FeedbackForm handleAdd={addFeedback} />
-                            <FeedbackStats feedback={feedback} />
-                            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-                        </>
-                    }>
-                    </Route>
-                    <Route path="/about" element={<AboutPage />} />
-                </Routes>
-                <AboutIconLink />
-            </div>
-        </Router>
+        <FeedbackProvider>
+            <Router>
+                <Header text='Feedback App' />
+                <div className="container">
+                    <Routes>
+                        <Route exact path="/" element= {
+                            <>
+                                <FeedbackForm />
+                                <FeedbackStats />
+                                <FeedbackList />
+                            </>
+                        }>
+                        </Route>
+                        <Route path="/about" element={<AboutPage />} />
+                    </Routes>
+                    <AboutIconLink />
+                </div>
+            </Router>
+        </FeedbackProvider>
     )
 }
 
--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import Card from './shared/Card';
 import PropTypes from 'prop-types';
 import { FaTimes } from 'react-icons/fa';
+import FeedbackContext from './context/FeedbackContext';
 
-function FeedbackItem({item, handleDelete}) {
+function FeedbackItem({item}) {
+
+    const {deleteFeedback} = useContext(FeedbackContext);
 
     return (
         <Card reverse={true}>
             <div className="num-display">{item.rating}</div>
-            <button className="close" onClick={(() => handleDelete(item.id))}>
+            <button className="close" onClick={(() => deleteFeedback(item.id))}>
                 <FaTimes color="purple" />
             </button>
             <div className="text-display">
